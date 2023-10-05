826c1768e9813f7c2d19ab63e005d329ee676f6d
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-10-05 15:46:24 -0400

    git init

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 // Seting Up Server
 const app = express();
-const PORT = process.env.Port || 8000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
