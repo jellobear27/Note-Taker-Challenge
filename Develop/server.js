734c207808d8511020be98a7eed111cad47b1ec2
Janell Smith <119452949+jellobear27@users.noreply.github.com>
2023-10-09 23:48:18 -0400

    fixed bugs

--- Develop/server.js
+++ Develop/server.js
@@ -8,19 +8,19 @@ const util = require("util");
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-// Seting Up Server
+// Set up Server
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Static Middleware
-app.use(express.static("./develop/public"));
+// Static Middleware
+app.use(express.static(path.join(__dirname, "public")));
 
 // API Route | "GET" request
 app.get("/api/notes", function (req, res) {
-  readFileAsync("./develop/db/db.json", "utf8").then(function (data) {
+  readFileAsync(path.join(__dirname, "db", "db.json"), "utf8").then(function (data) {
     notes = [].concat(JSON.parse(data));
     res.json(notes);
   });
@@ -28,28 +28,29 @@ app.get("/api/notes", function (req, res) {
 
 // API Route | "POST" request
 app.post("/api/notes", function (req, res) {
-  const note = req.body;
-  readFileAsync("./develop/db/db.json", "utf8")
+  const newNote = req.body;
+  readFileAsync(path.join(__dirname, "db", "db.json"), "utf8")
     .then(function (data) {
-      const note = [].concat(JSON.parse(data));
-      note.id = notes.length + 1;
-      notes.push(note);
+      const notes = [].concat(JSON.parse(data));
+      newNote.id = notes.length + 1;
+      notes.push(newNote);
       return notes;
     })
     .then(function (notes) {
-      writeFileAsync("./develop/db/db.json", JSON.stringify(notes));
-      res.json(note);
+      writeFileAsync(path.join(__dirname, "db", "db.json"), JSON.stringify(notes));
+      res.json(newNote);
     });
 });
+
 
 // API Route | "Delete" request
 app.delete("/api/notes/:id", function (req, res) {
   const idToDelete = parseInt(req.params.id);
-  readFileAsync("./develop/db/db.json", "utf8")
+  readFileAsync(path.join(__dirname, "db", "db.json"), "utf8")
     .then(function (data) {
-      const notes = [].concat(Json.parse(data));
+      const notes = [].concat(JSON.parse(data));
       const newNotesData = [];
-      for (let i = 0; i < notes.length; ) {
+      for (let i = 0; i < notes.length; i++) {
         if (idToDelete !== notes[i].id) {
           newNotesData.push(notes[i]);
         }
@@ -57,18 +58,18 @@ app.delete("/api/notes/:id", function (req, res) {
       return newNotesData;
     })
     .then(function (notes) {
-      writeFileAsync("./develop/db/db.json", JSON.stringify(notes));
+      writeFileAsync(path.join(__dirname, "db", "db.json"), JSON.stringify(notes));
       res.send("saved success!");
     });
 });
 
 // HTML Routes
 app.get("/notes", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Listening
